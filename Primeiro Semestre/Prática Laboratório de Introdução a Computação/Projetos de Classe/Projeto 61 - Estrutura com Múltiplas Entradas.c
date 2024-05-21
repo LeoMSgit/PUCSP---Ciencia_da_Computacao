7260825e150e5c1a62a138af9ac647e1aab58749
Leonardo Miguel dos Santos <leoms@usp.br>
2024-05-21 11:48:04 -0300

    Update Projeto 61 - Estrutura com Múltiplas Entradas.c

--- Primeiro Semestre/Prática Laboratório de Introdução a Computação/Projetos de Classe/Projeto 61 - Estrutura com Múltiplas Entradas.c
+++ Primeiro Semestre/Prática Laboratório de Introdução a Computação/Projetos de Classe/Projeto 61 - Estrutura com Múltiplas Entradas.c
@@ -18,42 +18,40 @@ void fDados(char n[40], char c[12], int i) {
     printf("------------------------------------------------------------------------------------\n");
 }
 
-void fClear() {
+void fClear() {							// Descarta os caracteres do buffer de entrada
     int c;
-    while ((c = getchar()) != '\n' && c != EOF) {
-        // Descarta os caracteres do buffer de entrada
+    while ((c = getchar()) != '\n' && c != EOF) {  
     }
 }
 
 int main() {
-	int j;
-    struct stamigo {  // Definição da estrutura
+	int i;
+    struct stamigo {  					// Definição da estrutura
         char nome[40];
         char celular[12];
         int idade;
     } amigo;
 	
 
-	while (1 && j < 4){	
+	while (i < 50){	
         printf("\nNome (Enter para SAIR): ");
         gets(amigo.nome);
-        j = j+1;
 
         // Verifica se o nome é vazio (apenas Enter pressionado)
-        if (amigo.nome[0] == '\0') {
-            break;  // Sai do loop se o usuário pressionar apenas Enter
+        if (amigo.nome[0] == '\0') {	// Verificador fim da String
+            break;  					// Sai do loop se o usuário pressionar apenas Enter
         }
 
         printf("\nCelular: ");
-        scanf("%11s", amigo.celular);  // Limita a entrada a 11 caracteres mais o terminador nulo
-        fClear();  // Limpa o buffer de entrada
+        scanf("%11s", amigo.celular);  	// Limita a entrada a 11 caracteres mais o terminador nulo
+        fClear(); 					  	// Limpa o buffer de entrada
 
         printf("\nIdade: ");
         scanf("%d", &amigo.idade);
-        fClear();  // Limpa o buffer de entrada
-
+        fClear();  					   	// Limpa o buffer de entrada
+		i = i+1;						// Contador i
         fDados(amigo.nome, amigo.celular, amigo.idade);
     }
-    printf("\n--------------------------------------");
+    printf("\n--------------------------------");
     printf("\nSAINDO");
 }
