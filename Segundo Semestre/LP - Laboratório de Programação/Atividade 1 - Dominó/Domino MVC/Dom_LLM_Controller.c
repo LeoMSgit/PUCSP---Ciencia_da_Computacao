a87fab8d9dc97575b73668af6da39a5e9b169ef0
Leonardo Miguel dos Santos <leoms@usp.br>
2024-08-28 09:59:44 -0300

    Add files via upload

---
+++ Segundo Semestre/LP - Laboratório de Programação/Atividade 1 - Dominó/Domino MVC/Dom_LLM_Controller.c
@@ -0,0 +1,11 @@
+// Dom_LLM_Controller.h
+#ifndef DOM_LLM_CONTROLLER_H
+#define DOM_LLM_CONTROLLER_H
+
+#include "Dom_LLM_Model.h"
+#include "Dom_LLM_View.h"
+
+// Funções do controlador
+void iniciarJogo();
+
+#endif // DOM_LLM_CONTROLLER_H
