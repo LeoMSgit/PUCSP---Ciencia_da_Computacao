f41ac75dec43cc86fbe0950094439f28308b6b05
Leonardo Miguel dos Santos <leoms@usp.br>
2024-08-28 11:20:07 -0300

    Add files via upload

---
+++ Segundo Semestre/LP - Laboratório de Programação/Atividade 1 - Dominó/Domino MVC - 2.0/Dom_LLM_Model.h
@@ -0,0 +1,25 @@
+// Dom_LLM_Model.h
+
+#ifndef DOM_LLM_MODEL_H
+#define DOM_LLM_MODEL_H
+
+#define TOTAL_PECAS 28
+
+typedef struct {
+    int lado1;
+    int lado2;
+} PecaDomino;
+
+typedef struct {
+    PecaDomino ordenadas[TOTAL_PECAS];
+    PecaDomino embaralhadas[TOTAL_PECAS];
+} TipoDomino;
+
+// Declaração externa da variável global
+extern TipoDomino tipo;
+
+// Funcoes do model
+void gerarPecas(PecaDomino pecas[]);
+void embaralharPecas(PecaDomino pecas[], int tamanho);
+
+#endif // DOM_LLM_MODEL_H
---
+++ Segundo Semestre/LP - Laboratório de Programação/Atividade 1 - Dominó/Domino MVC - 2.0/Dom_LLM_View.c
@@ -0,0 +1,55 @@
+// Dom_LLM_View.c
+
+#include "Dom_LLM_View.h"
+#include <stdio.h>
+
+int i, j;
+
+void imprimirPecas(PecaDomino pecas[], int tamanho) {
+    for (i = 0; i <= 6; i++) {
+        for (j = 0; j < tamanho; j++) {
+            if (pecas[j].lado1 == i) {
+                printf("[%d|%d] ", pecas[j].lado1, pecas[j].lado2);
+            }
+        }
+        printf("\n");
+    }
+}
+
+void exibirMenu(TipoDomino *tipo) {
+    int opcao;
+
+    do {
+        printf("Escolha uma opcao:\n");
+        printf("1. Exibir pecas em ordem\n");
+        printf("2. Exibir pecas embaralhadas ou reembaralhar pecas\n");
+        printf("3. Exibir pecas em ordem e embaralhadas\n");
+        printf("0. Sair\n");
+        printf("Digite sua opcao: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                printf("Pecas do Domino (Em Ordem):\n");
+                imprimirPecas(tipo->ordenadas, TOTAL_PECAS);
+                break;
+            case 2:
+                printf("Pecas do Domino (Embaralhadas):\n");
+                embaralharPecas(tipo->embaralhadas, TOTAL_PECAS);
+                imprimirPecas(tipo->embaralhadas, TOTAL_PECAS);
+                break;
+            case 3:
+                printf("Pecas do Domino (Em Ordem):\n");
+                imprimirPecas(tipo->ordenadas, TOTAL_PECAS);
+                printf("Pecas do Domino (Embaralhadas):\n");
+                imprimirPecas(tipo->embaralhadas, TOTAL_PECAS);
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida. Tente novamente.\n");
+        }
+        printf("\n");
+    } while (opcao != 0);
+}
