daa2685a24a309ba1c26e1153c1c4f3085d5a93d
Leonardo Miguel dos Santos <leoms@usp.br>
2024-08-28 09:53:59 -0300

    Update Dom_LLM_Controller.c

--- Segundo Semestre/LP - Laboratório de Programação/Atividade 1 - Dominó/Domino MVC/Dom_LLM_Controller.c
+++ Segundo Semestre/LP - Laboratório de Programação/Atividade 1 - Dominó/Domino MVC/Dom_LLM_Controller.c
@@ -1,11 +1,36 @@
-// Dom_LLM_Controller.h
-#ifndef DOM_LLM_CONTROLLER_H
-#define DOM_LLM_CONTROLLER_H
-
-#include "Dom_LLM_Model.h"
+// Dom_LLM_Controller.v
+#include <stdio.h>
+#include "Dom_LLM_Controller.h"
 #include "Dom_LLM_View.h"
 
-// Funções do controlador
-void iniciarJogo();
+void exibirMenu(TipoDomino *tipo) {
+    int opcao;
 
-#endif // DOM_LLM_CONTROLLER_H
+    do {
+        printf("Escolha uma opcao:\n");
+        printf("1. Exibir pecas em ordem\n");
+        printf("2. Exibir pecas embaralhadas ou reembaralhar pecas\n");
+        printf("3. Exibir pecas em ordem e embaralhadas\n");
+        printf("0. Sair\n");
+        printf("Digite sua opcao: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                mostrarPecasEmOrdem(tipo);
+                break;
+            case 2:
+                mostrarPecasEmbaralhadas(tipo);
+                break;
+            case 3:
+                mostrarPecasEmOrdemEEmbaralhadas(tipo);
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida. Tente novamente.\n");
+        }
+        printf("\n");
+    } while (opcao != 0);
+}
