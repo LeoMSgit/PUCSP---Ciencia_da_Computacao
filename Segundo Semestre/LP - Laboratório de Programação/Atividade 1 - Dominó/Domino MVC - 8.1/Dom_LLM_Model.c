1004ed068d257146c97f9e84535f733c46f4ed54
Leonardo Miguel dos Santos <leoms@usp.br>
2024-09-10 20:09:02 -0300

    Add files via upload

---
+++ Segundo Semestre/LP - Laboratório de Programação/Atividade 1 - Dominó/Domino MVC - 8.1/Dom_LLM_Model.c
@@ -0,0 +1,42 @@
+// Dom_LLM_Model.h
+
+#include "Dom_LLM_Model.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+// Definição das variáveis globais
+TipoDomino tipo;
+int numeroJogadores = MIN_JOGADORES; // Inicializa com o mínimo de jogadores
+Jogador jogadores[MAX_JOGADORES];
+
+void gerarPecas(PecaDomino pecas[]) {
+    int indice = 0;
+    
+    for (int i = 0; i <= 6; i++) {
+        for (int j = i; j <= 6; j++) {
+            pecas[indice].lado1 = i;
+            pecas[indice].lado2 = j;
+            indice++;
+        }
+    }
+}
+
+void embaralharPecas(PecaDomino pecas[], int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        int sorteio = rand() % tamanho;
+        PecaDomino temp = pecas[i];
+        pecas[i] = pecas[sorteio];
+        pecas[sorteio] = temp;
+    }
+}
+
+void distribuirPecas(Jogador jogadores[], PecaDomino pecasEmbaralhadas[], int numJogadores) {
+    int indicePeca = 0;  // Índice para rastrear de onde tirar as peças embaralhadas
+    
+    for (int i = 0; i < numJogadores; i++) {
+        for (int j = 0; j < PECAS_POR_JOGADOR; j++) {
+            jogadores[i].pecas[j] = pecasEmbaralhadas[indicePeca];  // Atribui a peça ao jogador
+            indicePeca++;
+        }
+    }
+}
---
+++ Segundo Semestre/LP - Laboratório de Programação/Atividade 1 - Dominó/Domino MVC - 8.1/Dom_LLM_PROJETO.c
@@ -0,0 +1,12 @@
+//DOM-LLM - Projeto Dominó – Etapa 3 - REQ08
+//28/08/2024 - Grupo: LLM
+//Leonardo Miguel dos Santos
+//Luiz Fernando De Marchi Andrade
+
+//Dom_LLM_Projeto.c
+
+#include "Dom_LLM_Controller.c"
+
+void main() {
+    iniciarJogo();
+}
