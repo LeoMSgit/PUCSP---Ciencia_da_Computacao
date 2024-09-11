f9523cb0335150c22d13683b48cdc352fbdf7b6e
Leonardo Miguel dos Santos <leoms@usp.br>
2024-09-11 11:52:45 -0300

    Add files via upload

--- Segundo Semestre/LP - Laboratório de Programação/Atividade 1 - Dominó/Atividades Classe/Atividade 01 - Criação e manipulação de FILE (fprt, fopen, fputs e fclose).c
+++ Segundo Semestre/LP - Laboratório de Programação/Atividade 1 - Dominó/Atividades Classe/Atividade 01 - Criação e manipulação de FILE (fprt, fopen, fputs e fclose).c
@@ -11,7 +11,7 @@ int main(){
 	fprt = fopen ("Text.txt", "w"); 			//Designa o valor de fopen (file open - oq está dentro daquele file) 
 											//W serve para comandar a writing ou criação do arquivo Text.txt
 	fputs ("Bom dia TROPAAAAA!\n", fprt); 	//Insere o texto no arquivo em questão, que está armazenado no fpointer
-	
+	fputs ("* ☆* ☆* ☆*", fprt);
 	fclose(fprt);							//Limpa o buffer de texto, eliminando qualquer informação gravada no fpointer
 	return 0;
 }
