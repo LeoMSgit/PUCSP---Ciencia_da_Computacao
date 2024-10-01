777113ead8432411bd5daa519a52ec3d366a1acb
Leonardo Miguel dos Santos <leoms@usp.br>
2024-10-01 10:18:16 -0300

    Add files via upload

---
+++ Segundo Semestre/LP - Laboratório de Programação/Atividade 1 - Dominó/Domino MVC - 17.2/Dom_LLM_PROJETO.c
@@ -0,0 +1,13 @@
+//DOM-LLM - Projeto Dominó – Etapa 4 - REQ16
+//28/08/2024 - Grupo: LLM
+//Leonardo Miguel dos Santos
+//Luiz Fernando De Marchi Andrade
+
+//Dom_LLM_Projeto.c
+
+#include "Dom_LLM_Controller.c"
+
+int main() {
+    iniciarJogo();
+    return 0;
+}
