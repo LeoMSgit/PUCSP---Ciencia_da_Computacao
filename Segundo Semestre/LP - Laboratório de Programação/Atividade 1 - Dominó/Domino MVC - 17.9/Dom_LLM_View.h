2809df732774aa41a6b55674c9eb54790b9ef5fa
Leonardo Miguel dos Santos <leoms@usp.br>
2024-10-02 09:43:09 -0300

    Add files via upload

---
+++ Segundo Semestre/LP - Laboratório de Programação/Atividade 1 - Dominó/Domino MVC - 17.9/Dom_LLM_View.h
@@ -0,0 +1,25 @@
+// Dom_LLM_View.h
+
+#ifndef DOM_LLM_VIEW_H
+#define DOM_LLM_VIEW_H
+
+// Define numero de jogadas por jogador (Fins de Dubuggin)
+#define LIMITE_JOGADAS 1
+
+#include "Dom_LLM_Model.h"
+
+// Funções da View
+void imprimirPecas(PecaDomino pecas[], int tamanho);
+void exibirMenuInicial();
+void exibirMenuGeral(TipoDomino tipo);
+void exibirMenuAcoesJogador();
+void exibirMesa();
+void imprimirPecasMao();
+void comprarPeca(Jogador jogadores[], int indiceJogador);
+int jogadorInicial();
+int jogadaValida(PecaDomino peca, int ladoEscolhido);
+void adicionarPecaMesa(PecaDomino peca, int ladoEscolhido);
+void salvarJogo();
+void abandonarPartida(int jogadorAtual);
+
+#endif // DOM_LLM_VIEW_H
