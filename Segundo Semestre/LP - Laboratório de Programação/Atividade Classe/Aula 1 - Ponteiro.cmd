13bffb929fa973510187ff4e66171532617481ae
Leonardo Miguel dos Santos <leoms@usp.br>
2024-10-23 12:00:21 -0300

    Create Aula 1 - Ponteiro.cmd

---
+++ Segundo Semestre/LP - Laboratório de Programação/Atividade Classe/Aula 1 - Ponteiro.cmd
@@ -0,0 +1,7 @@
+Ponteiros 
+char ch = 'x' -> Define a variável ch como tipo char e valor x
+char *prt = &ch -> Define o ponteiro prt como tipo char e o valor referente Á POSIÇÃO ONDE A VARIÁVEL ch ESTÁ ALOCADA
+
+O prt indica o endereço para qual ele aponta
+O &ch indica o endereço da variável
+O *prt indica o conteúdo da variável
