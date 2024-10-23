1774d56f9c01e501a97255c9fcacb65ae3f33c6f
Leonardo Miguel dos Santos <leoms@usp.br>
2024-10-23 12:16:45 -0300

    Update Aula 1 - Ponteiro.cmd

--- Segundo Semestre/LP - Laboratório de Programação/Atividade Classe/Aula 1 - Ponteiro.cmd
+++ Segundo Semestre/LP - Laboratório de Programação/Atividade Classe/Aula 1 - Ponteiro.cmd
@@ -5,3 +5,15 @@ char *prt = &ch -> Define o ponteiro prt como tipo char e o valor referente Á P
 O prt indica o endereço para qual ele aponta
 O &ch indica o endereço da variável
 O *prt indica o conteúdo da variável
+
+#include <stdio.h>
+
+int main() {
+	int a = 100;
+    int *ptr = &a;
+    printf("%d, %d, %d", *ptr, a, ptr);
+}
+
+Nesse caso     *ptr retorna 100, pois está apontando o conteúdo do endereço da variável a
+                a retorna 100, pois é o conteúdo da variável a
+                ptr retorna 6684180, pois está apontando o endereço onde a variável a está registrada
