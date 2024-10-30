bb4178102d45981ab9959390134c5611d8c2e051
Leonardo Miguel dos Santos <leoms@usp.br>
2024-10-30 10:13:16 -0300

    Update and rename Aula 2 - Estrutura de Dados.md to Aula 2 - Estrutura de Dados.c

---
+++ Segundo Semestre/LP - Laboratório de Programação/Atividade Classe/Aula 2 - Estrutura de Dados.c
@@ -0,0 +1,22 @@
+Estrutura de Dados
+
+Filas 
+- São estrutras que permitem a inserção e remoção de dados na ordem FIFO (First in-First Out)
+   - Os elementos são inseridos em ordem de chegada, o primiero inserido fica na primeira posição, o segundo inserido na segunda posição e assim por diante
+      - A extremidade de inserção é a 'rear', é o último local disponível da fila
+   - Para a ordem de retirada, a ordem de retirada é a mesma de insersão, o primeiro a ser removido é o elemento na primeira posição e assim por diante
+      - A extremidade de remoção é 'front', é sempre o primeiro elemento da lista
+- A implementação de filas é feita por um vetor[0 até N-1], sendo que parte desse vetor é a nossa fila, delimitada pelo segmento fila[p até u-1]
+   - O primeiro elemento da fila é representado por 'p' e o útlumo 'u-1'
+   - A fila é considerada vazia se 'p == u' e cheia se 'u == N'
+
+  
+  EXEMPLOS
+
+  int tirafila (void){
+    return fila[p++];
+  }
+
+  void colocafila (int y){
+    fila[u++] = y;
+  }
--- Segundo Semestre/LP - Laboratório de Programação/Atividade Classe/Aula 2 - Estrutura de Dados.md
+++
@@ -1,2 +0,0 @@
-Estrutura de Dados
-
