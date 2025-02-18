af65bc6119ff6134f266dc5a340e625a420d359b
Leonardo Miguel dos Santos <leoms@usp.br>
2025-02-18 08:48:19 -0300

    Create Exercício 3.c

---
+++ Terceiro Semestre/EDL - Estrutura de Dados Lineares/Exercício de OPERADORES RELACIONAIS – FEV 2025/Exercício 3/Exercício 3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+	bool ok;
+	int A;
+	A = 0;
+	
+	if (A=0) {
+		ok = true;}
+			else {
+				ok = false;}
+		
+	if (ok) {
+		printf("\n A igual a zero \n");}
+			else printf("\n A diferente de zero \n");
+}
